49aa7a67f031dfe4109721d41cab93f265a1d212
ramu <ramu@ram.com>
2023-01-05 21:58:32 +0530

    manual authentication done

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,25 @@
 const User=require('../models/user')
 exports.profile=function(req,res)
+{   
+   const a=req.cookies.islogin;
+   
+   if(a=='false'){  return res.redirect('/user/signin');}
+    const userid=req.cookies.user;
+    
+   User.findById(userid,function(err,man){
+        console.log(man.email);
+    if(err){console.log('errror');}
+ 
+    res.render('profile.ejs',{email:man.email});
+
+    
+
+})
+
+}
+exports.user=function(req,res)
 {
-    res.end('<h1>this is the user profile</h1>')
+    res.render('user')
 }
 exports.friends=function(req,res)
 {
@@ -23,4 +41,39 @@ exports.create=function(req,res)
 {
    User.create(req.body,function(err,newc){if(err){console.error('there is an error in creating the user');return;}console.log(`${newc} \n created`)})
    res.redirect('/user/signin');
+}
+exports.signout=function(req,res)
+{
+   res.cookie('user',undefined);
+  res.cookie('islogin',false);
+   res.redirect('/user/signin');
+  
+}
+exports.auth=function(req,res)
+{
+      
+       
+        User.findOne({email:req.body.email},function(err,user)
+        {
+            if(err)return;
+            if(!user){return;}
+            else{
+               
+                if(user.password!=req.body.password){
+                
+                  return res.end("<h1>wrong password</h1>");
+                
+                }
+                else{
+                    res.cookie('user',user.id);
+                    res.cookie('islogin',true);
+                   
+                    return res.redirect('/user/profile');
+                }
+            }
+        })
+    
+
+      
+
 }
